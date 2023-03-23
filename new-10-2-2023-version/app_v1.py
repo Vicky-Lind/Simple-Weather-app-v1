c424f54f0659e35c4650f17b1e7b1f05585a297f
VictoriaLind <victoria@lindstar.fi>
2023-03-23 14:47:50 +0200

    fixes, probably done for now 23.3.2023

--- new-10-2-2023-version/app_v1.py
+++ new-10-2-2023-version/app_v1.py
@@ -63,8 +63,11 @@ class mainWindow(QMainWindow):
         #Make the frame have a modern, gradient background
         self.mainFrm.setStyleSheet("""
             QFrame {
-                background-color: rgba(151, 190, 252, 100);
-                border-radius: 15px;
+                background: QLinearGradient( x1: 0, y1: 0,
+                                            x2: 0, y2: 1, 
+                                            stop: 0 #5a4bf0, 
+                                            stop: 1 #85c4f9 );
+                border-radius: 30px;
             }
         """)
 
@@ -97,17 +100,16 @@ class mainWindow(QMainWindow):
 #-----------..rest of the elements, add blur(?), etc.-
 
         self.mainLbl = QLabel(self)
-        self.mainLbl.setGeometry(10, 20, 220, 420)
+        self.mainLbl.setGeometry(10, 280, 220, 140)
         self.mainLbl.setAlignment(QtCore.Qt.AlignCenter)
-        
         #  # creating a blur effect
         # self.blur_effect = QGraphicsBlurEffect()
         # # adding blur effect to the label
         # self.mainLbl.setGraphicsEffect(self.blur_effect)
         self.mainLbl.setStyleSheet("""
             QLabel {
-                background-color: rgba(255, 255, 255, 80);
                 border-radius: 15px;
+                background-color: rgb(255, 255, 255, 125)
             }
         """)
 
@@ -115,7 +117,7 @@ class mainWindow(QMainWindow):
         self.closeBtn = QPushButton(self)
         self.closeBtn.setIcon(QIcon('images/test-removebg.png'))
         self.closeBtn.setIconSize(QtCore.QSize(20, 20))
-        self.closeBtn.setGeometry(195, -5, 50, 30)
+        self.closeBtn.setGeometry(180, -5, 50, 30)
         self.closeBtn.clicked.connect(sys.exit)
         self.closeBtn.setStyleSheet("""
             QPushButton {
@@ -137,17 +139,17 @@ class mainWindow(QMainWindow):
         self.cityText = str()
         self.cityEntry = QLineEdit(self, text=self.cityText)
         self.cityEntry.setPlaceholderText("Enter city name")
-        self.cityEntry.setGeometry(10, 19, 160, 30)
+        self.cityEntry.setGeometry(10, 19, 195, 30)
         self.cityEntry.setStyleSheet("""
             QLineEdit {
                 background-color: rgba(255, 255, 255, 60);
-                border-top-left-radius: 10px;
+                border-top-left-radius: 15px;
                 border-bottom-left-radius: 10px;
-                padding-left: 5px;
+                padding-left: 12px;
                 padding-right: 5px;
                 color: white;
                 font-family: Yu Gothic UI Light;
-                font-size: 14px;
+                font-size: 15px;
             }
             Active {
                 background-color: red;
@@ -157,17 +159,17 @@ class mainWindow(QMainWindow):
         self.searchBtn = QPushButton(self)
         self.searchBtn.setIcon(QIcon('images/search-white-removebg.png'))
         self.searchBtn.setIconSize(QtCore.QSize(20, 20))
-        self.searchBtn.setGeometry(170, 15, 24, 30)
+        self.searchBtn.setGeometry(205, 19, 24, 30)
         self.searchBtn.clicked.connect(self.search)
 
-        self.shortcut = QShortcut(QKeySequence("Shift+Enter"), self)
+        self.shortcut = QShortcut(QKeySequence("Shift+Return"), self)
         self.shortcut.activated.connect(self.search)
 
         # self.searchBtn.setStyleSheet("background-color: rgba(255, 255, 255, 60); border-top-right-radius: 10px; border-bottom-right-radius: 10px; padding-right: 5px;")
         self.searchBtn.setStyleSheet("""
             QPushButton {
                 background-color: rgba(255, 255, 255, 60);
-                border-top-right-radius: 10px;
+                border-top-right-radius: 15px;
                 border-bottom-right-radius: 10px;
                 padding-right: 5px;
                 padding-left: 5px;
@@ -178,57 +180,64 @@ class mainWindow(QMainWindow):
         """)
  
         self.locationLbl = QLabel(self, text='')
-        self.locationLbl.setGeometry(26, 45, 200, 30)
+        self.locationLbl.setGeometry(80, 55, 200, 30)
         self.locationLbl.setStyleSheet("""
             QLabel {
                 color: white;
                 font-family: Yu Gothic UI Light;
-                font-size: 17px;
+                font-size: 14px;
+                text-align: center;
             }
         """)
 
         self.tempLbl = QLabel(self, text='')
-        self.tempLbl.setGeometry(10, 80, 200, 50)
+        self.tempLbl.setGeometry(100, 110, 220, 70)
         self.tempLbl.setStyleSheet("""
             QLabel {
                 color: white;
                 font-family: Yu Gothic UI Light;
-                font-size: 50px;
+                font-size: 75px;
             }
         """)
 
         self.image = QLabel(self)
         self.image.setAlignment(QtCore.Qt.AlignCenter)
         self.image.setScaledContents(True)
-        self.image.setGeometry(80, 150, 120, 120)
+        self.image.setGeometry(10, 95, 100, 100)
 
         self.weatherLbl = QLabel(self, text='')
-        self.weatherLbl.setGeometry(10, 340, 200, 30)
+        self.weatherLbl.setGeometry(77, 180, 200, 30)
         self.weatherLbl.setStyleSheet("""
             QLabel {
                 color: white;
                 font-family: Yu Gothic UI Light;
-                font-size: 17px;
+                font-size: 15px;
+                text-align: center;
+                font-weight: bold;
             }
         """)
 
         self.feelsLikeLbl = QLabel(self, text='')
-        self.feelsLikeLbl.setGeometry(10, 370, 200, 30)
+        self.feelsLikeLbl.setGeometry(70, 300, 200, 30)
         self.feelsLikeLbl.setStyleSheet("""
             QLabel {
                 color: white;
                 font-family: Yu Gothic UI Light;
-                font-size: 17px;
+                font-size: 15px;
+                text-align: center;
+                font-weight: bold;
             }
         """)
 
         self.windLbl = QLabel(self, text='')
-        self.windLbl.setGeometry(10, 400, 200, 30)
+        self.windLbl.setGeometry(82, 365, 200, 30)
         self.windLbl.setStyleSheet("""
             QLabel {
                 color: white;
                 font-family: Yu Gothic UI Light;
-                font-size: 17px;
+                font-size: 15px;
+                text-align: center;
+                font-weight: bold;
             }
         """)
 
@@ -246,12 +255,12 @@ class mainWindow(QMainWindow):
         self.drag_start_pos = None
 
         self.timeLbl = QLabel(self, text='')
-        self.timeLbl.setGeometry(10, 220, 200, 30)
+        self.timeLbl.setGeometry(95, 70, 200, 30)
         self.timeLbl.setStyleSheet("""
             QLabel {
                 color: white;
                 font-family: Yu Gothic UI Light;
-                font-size: 17px;
+                font-size: 11px;
             }
         """)
 
@@ -273,28 +282,31 @@ class mainWindow(QMainWindow):
 #------This fetches data from the API------#
 
     def get_weather(self, city):
-        result = requests.get(url.format(city, api_key))
-        # print(url.format(city, api_key))
-        
-        if result:
-            json = result.json()
-        #(City, country, temp_celsius, feels_like, icon, weather, longitude, latitude)
-            city = json['name']
-            country = json['sys']['country']
-            temp_kelvin = json['main']['temp']
-            temp_celsius = temp_kelvin - 273.15
-            feels_like = json['main']['feels_like'] - 273.15
-            wind = json['wind']['speed']
-            icon = json['weather'][0]['icon']
-            weather = json['weather'][0]['description']
-            longitude = json['coord']['lon']
-            latitude = json['coord']['lat']
+        try:
+            result = requests.get(url.format(city, api_key))
+            # print(url.format(city, api_key))
+            
+            if result:
+                json = result.json()
+            #(City, country, temp_celsius, feels_like, icon, weather, longitude, latitude)
+                city = json['name']
+                country = json['sys']['country']
+                temp_kelvin = json['main']['temp']
+                temp_celsius = temp_kelvin - 273.15
+                feels_like = json['main']['feels_like'] - 273.15
+                wind = json['wind']['speed']
+                icon = json['weather'][0]['icon']
+                weather = json['weather'][0]['description']
+                longitude = json['coord']['lon']
+                latitude = json['coord']['lat']
 
-            final = (city, country, temp_celsius, feels_like, wind, icon, weather, longitude, latitude)
-            return final
-        else:
+                final = (city, country, temp_celsius, feels_like, wind, icon, weather, longitude, latitude)
+                return final
+            else:
+                print("Error while getting weather data")
+                return None
+        except Exception as e:
             print("Error while getting weather data")
-            return None
 #------------------------------------------------
 
 #################################################
@@ -306,41 +318,43 @@ class mainWindow(QMainWindow):
 #-------THE SAME AS TEMP, IF YES, THEN DONT SHOW?------#
 
     def search(self):
-        city = self.cityEntry.text()
-        weather = self.get_weather(city)
-  
-        if weather:
-            self.locationIcon.setPixmap(QPixmap('images/location_v2_16x.png'))
-            self.locationIcon.setGeometry(10, 53, 16, 16)
-            self.locationIcon.setScaledContents(True)
-            self.locationIcon.setAlignment(QtCore.Qt.AlignCenter)
-            self.locationIcon.setStyleSheet("background-color: transparent;")
-
-            self.locationLbl.setText('{}, {}'.format(weather[0], weather[1]))
-            
-            pixmap = QPixmap('images/more_weather_icons/{}.png'.format(weather[5]))  
-            self.image.setPixmap(pixmap)
-
-            self.tempLbl.setText('{:.0f}°'.format(weather[2]))
-            
-            self.feelsLikeLbl.setText('{:.0f}°'.format(weather[3]))
-            
-            self.windLbl.setText('{:.0f}km/h'.format(weather[4]))
-
-            self.weatherLbl.setText(weather[6])
-
-            self.time = TimezoneFinder().timezone_at(lng=weather[7], lat=weather[8])
-            self.timeLbl.setText(self.time)
-            # print(self.time)
-            self.timezone = timezone(self.time)
-            self.time = datetime.now(self.timezone)
-            self.timeLbl.setText(self.time.strftime('%H:%M'))
-            # print(self.time.strftime('%H:%M'))
-
-            QtCore.QTimer.singleShot(3000, self.search)
-        else:
-            QMessageBox.critical(self, 'Error', "Cannot find city {}".format(city))
+        try:
+            city = self.cityEntry.text()
+            weather = self.get_weather(city)
     
+            if weather:
+                self.locationIcon.setPixmap(QPixmap('images/locationImage.png'))
+                self.locationIcon.setGeometry(67, 65, 13, 13)
+                self.locationIcon.setScaledContents(True)
+                self.locationIcon.setAlignment(QtCore.Qt.AlignCenter)
+                self.locationIcon.setStyleSheet("background-color: transparent; text-align: center;")
+
+                self.locationLbl.setText('{}, {}'.format(weather[0], weather[1]))
+                
+                pixmap = QPixmap('images/weather_icons_orig/{}.png'.format(weather[5]))  
+                self.image.setPixmap(pixmap)
+
+                self.tempLbl.setText('{:.0f}°'.format(weather[2]))
+                
+                self.feelsLikeLbl.setText('feels like {:.0f}°'.format(weather[3]))
+                
+                self.windLbl.setText('{:.0f}km/h'.format(weather[4]))
+
+                self.weatherLbl.setText(weather[6])
+
+                self.time = TimezoneFinder().timezone_at(lng=weather[7], lat=weather[8])
+                self.timeLbl.setText(self.time)
+                # print(self.time)
+                self.timezone = timezone(self.time)
+                self.time = datetime.now(self.timezone)
+                self.timeLbl.setText(self.time.strftime('%H:%M'))
+                # print(self.time.strftime('%H:%M'))
+
+                QtCore.QTimer.singleShot(3000, self.search)
+            else:
+                print(self, 'Error', "Cannot find city {}".format(city))
+        except Exception as e:
+            print("Error while getting weather data")
 #------------------------------------------------
 
 #################################################
